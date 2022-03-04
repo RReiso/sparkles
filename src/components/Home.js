6109cf944ab37072222ffe46ab7ceb7dfe707f75
Ruta Reisoglu <rutareiso@protonmail.com>
2022-03-03 23:11:02 -0500

    Style homepage for mobile screens

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,42 +6,53 @@ const Home = () => {
   const [img1, img2, img3, img4, img5, icon1, icon2, icon3, icon4, icon5] =
     homeImages;
   return (
-    <>
-      <h3>Because this Moment is Timeles ...</h3>
+    <main>
+      <h2 className="intro-line">Because this Moment is Timeless...</h2>
       <section className="home-images">
         <div>
           <img src={img2} alt="woman" />
         </div>
         <div>
-          <div>
-            <img src={img1} alt="woman" />
-            <img src={img3} alt="woman" />
+          <div className="home-images">
+            <img src={img1} alt="pearl necklase" />
+            <img src={img3} alt="rings with blue stones" />
           </div>
-          <div>
-            <img src={img4} alt="woman" />
-            <img src={img5} alt="woman" />
+          <div className="home-images">
+            <img src={img4} alt="ring with a yellow stone" />
+            <img src={img5} alt="diamonds" />
           </div>
         </div>
       </section>
-      <section className="icons">
-        <Link to="/new">
-          <img src={icon3} alt="woman" />
-          <p>new</p>
-        </Link>
-        <Link to="/luxury">
-          <img src={icon1} alt="woman" />
-          <p>luxury</p>
-        </Link>
-        <Link to="/gifts">
-          <img src={icon5} alt="woman" />
-          <p>gifts</p>
-        </Link>
-        <Link to="/sale">
-          <img src={icon2} alt="woman" />
-          <p>sale</p>
-        </Link>
+      <section>
+        <p className="explore">Explore our collection:</p>
+        <div className="icons">
+          <div className="icon">
+            <img src={icon3} alt="diamond" />
+            <Link to="/new" className="btn-secondary">
+              new
+            </Link>
+          </div>
+          <div className="icon">
+            <img src={icon1} alt="necklase" />
+            <Link to="/luxury" className="btn-secondary">
+              luxury
+            </Link>
+          </div>
+          <div className="icon">
+            <img src={icon5} alt="ring" />
+            <Link to="/gifts" className="btn-secondary">
+              gifts
+            </Link>
+          </div>
+          <div className="icon">
+            <img src={icon2} alt="pearl necklase" />
+            <Link to="/sale" className="btn-secondary">
+              sale
+            </Link>
+          </div>
+        </div>
       </section>
-    </>
+    </main>
   );
 };
 
