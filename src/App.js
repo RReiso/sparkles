b0658a8b6fe3d08f1d32d69a8dc8948647f5a2a8
Ruta Reisoglu <rutareiso@protonmail.com>
2022-03-07 14:20:24 -0500

    Scroll to top on route change

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Items from "./components/Items";
 import PageNotFound from "./components/PageNotFound";
-// import "./styles/App.scss";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/all" element={<Items />} />
-        <Route path="/new" element={<Items />} />
-        <Route path="/gifts" element={<Items />} />
-        <Route path="/luxury" element={<Items />} />
-        <Route path="/sale" element={<Items />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <ScrollToTop>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/all" element={<Items />} />
+          <Route path="/new" element={<Items />} />
+          <Route path="/gifts" element={<Items />} />
+          <Route path="/luxury" element={<Items />} />
+          <Route path="/sale" element={<Items />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </ScrollToTop>
     </BrowserRouter>
   );
 }
